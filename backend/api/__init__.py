219ca77f7544145065898e1738b3cf0813cd248b
tc-imba <liuyh615@sjtu.edu.cn>
2019-07-15 22:24:52 +0800

    dump new db

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -9,7 +9,6 @@ from backend.model import get_db, ObjectId, jsonify
 @check_argument("uid")
 def get_user_groups(uid):
     db = get_db()
-    # print(uid)
     pipeline = [{
         "$match": {
             "uid": ObjectId(uid)
@@ -33,3 +32,38 @@ def get_user_groups(uid):
     # print(jsonify(data))
     return jsonify(data)
 
+
+@app.route('/api/group')
+@query_argument
+@check_argument("gid")
+def get_group(gid):
+    db = get_db()
+
+
+@app.route('/api/group_users')
+@query_argument
+@check_argument("gid")
+def get_group_users(gid):
+    db = get_db()
+    pipeline = [{
+        "$match": {
+            "gid": ObjectId(gid)
+        }
+    }, {
+        "$lookup": {
+            "from": "users",
+            "localField": "uid",
+            "foreignField": "_id",
+            "as": "user"
+        }
+    }, {
+        "$group": {
+            "_id": "$gid",
+            "users": {"$push": {"$arrayElemAt": ["$user", 0]}}
+        }
+    }]
+    data = list(db.group_user.aggregate(pipeline))
+    if data:
+        data = data[0]["users"]
+    # print(jsonify(data))
+    return jsonify(data)

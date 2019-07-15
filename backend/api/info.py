e3bed7aa40dfe68bfec05d9a3103e1fffb3674d0
tc-imba <liuyh615@sjtu.edu.cn>
2019-07-15 22:51:29 +0800

    new message api

---
+++ backend/api/info.py
@@ -0,0 +1,69 @@
+# from flask import jsonify
+from backend import app
+from backend.utils import check_argument, query_argument
+from backend.model import get_db, ObjectId, jsonify
+
+
+@app.route('/api/groups')
+@query_argument
+@check_argument("uid")
+def get_user_groups(uid):
+    db = get_db()
+    pipeline = [{
+        "$match": {
+            "uid": ObjectId(uid)
+        }
+    }, {
+        "$lookup": {
+            "from": "groups",
+            "localField": "gid",
+            "foreignField": "_id",
+            "as": "group"
+        }
+    }, {
+        "$group": {
+            "_id": "$uid",
+            "groups": {"$push": {"$arrayElemAt": ["$group", 0]}}
+        }
+    }]
+    data = list(db.group_user.aggregate(pipeline))
+    if data:
+        data = data[0]["groups"]
+    # print(jsonify(data))
+    return jsonify(data)
+
+
+@app.route('/api/group')
+@query_argument
+@check_argument("gid")
+def get_group(gid):
+    db = get_db()
+
+
+@app.route('/api/group_users')
+@query_argument
+@check_argument("gid")
+def get_group_users(gid):
+    db = get_db()
+    pipeline = [{
+        "$match": {
+            "gid": ObjectId(gid)
+        }
+    }, {
+        "$lookup": {
+            "from": "users",
+            "localField": "uid",
+            "foreignField": "_id",
+            "as": "user"
+        }
+    }, {
+        "$group": {
+            "_id": "$gid",
+            "users": {"$push": {"$arrayElemAt": ["$user", 0]}}
+        }
+    }]
+    data = list(db.group_user.aggregate(pipeline))
+    if data:
+        data = data[0]["users"]
+    # print(jsonify(data))
+    return jsonify(data)
---
+++ backend/api/message.py
@@ -0,0 +1,14 @@
+from backend import app
+from backend.utils import check_argument, form_argument
+from backend.model import get_db, ObjectId, jsonify
+
+
+@app.route('/api/message')
+@form_argument
+@check_argument("uid")
+@check_argument("gid")
+@check_argument("outercypher")
+@check_argument("noncestr")
+@check_argument("ephermeralpubkey")
+def post_message(uid, gid, outercypher, noncestr, ephermeralpubkey):
+    print(uid, gid, outercypher, noncestr, ephermeralpubkey)

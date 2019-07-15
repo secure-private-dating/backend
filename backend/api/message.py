2fb7070ef4d9055d4ef888ef1c1f0aea0ad84b5e
Reapor-Yurnero <reapor.yurnero@gmail.com>
2019-07-15 23:09:03 +0800

    POST not successful

--- backend/api/message.py
+++ backend/api/message.py
@@ -3,12 +3,15 @@ from backend.utils import check_argument, form_argument
 from backend.model import get_db, ObjectId, jsonify
 
 
-@app.route('/api/message')
+@app.route('/api/message', methods=['POST'])
 @form_argument
 @check_argument("uid")
 @check_argument("gid")
-@check_argument("outercypher")
-@check_argument("noncestr")
-@check_argument("ephermeralpubkey")
-def post_message(uid, gid, outercypher, noncestr, ephermeralpubkey):
-    print(uid, gid, outercypher, noncestr, ephermeralpubkey)
+# @check_argument("outercypher")
+# @check_argument("noncestr")
+# @check_argument("ephermeralpubkey")
+# def post_message(uid, gid, outercypher, noncestr, ephermeralpubkey):
+#     print(uid, gid, outercypher, noncestr, ephermeralpubkey)
+def post_message(uid, gid):
+    print(uid, gid)
+    return jsonify([uid, gid])

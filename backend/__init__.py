7f237f53020125171b6ce6672aa013455e954e6e
tc-imba <liuyh615@sjtu.edu.cn>
2019-08-06 11:17:09 +0800

    add prefix 'backend'

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,7 +7,28 @@ from flask_socketio import SocketIO
 from flask_session import Session
 from backend.middleware import WeAppSessionFix
 
+
+class PrefixMiddleware(object):
+
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
+
 app = Flask(__name__)
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/backend')
+
+
 # app.wsgi_app = WeAppSessionFix(app.wsgi_app)
 
 app.config.from_object('backend.config')
@@ -25,6 +46,7 @@ sess.init_app(app)
 
 import backend.api
 import backend.socket
+
 
 #
 # @app.before_request
